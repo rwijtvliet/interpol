3e90654e8a093c56d274ac00b056ee7bae0ac5b4
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-11-07 23:30:54 +0000

    Update docs after building Travis build 7 of rwijtvliet/interpol
    The docs were built from the branch 'main' against the commit
    f1cf1fb905f274c8979a8a3ef06deea04122190c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/rwijtvliet/interpol/jobs/742155026.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs/build/html --no-require-master

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post8+gf29f9b0',
+    VERSION: '0.1.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
